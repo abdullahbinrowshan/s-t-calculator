d2516e25097fa5c3d771086529274a3ef8f805a1
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-03-06 10:54:34 +0600

    all html elements have received with data attribute

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello world');
+class Calculator {
+    constructor (previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+
+
+
+const numberButtons = document.querySelectorAll(['data-number']);
+const operationButtons = document.querySelectorAll(['data-operation']);
+const deleteButton = document.querySelectorAll(['data-delete']);
+const allClearButton = document.querySelectorAll(['data-all-clear']);
+const previousOperandButton = document.querySelectorAll(['data-previous-operand']);
+const currentOperandButton = document.querySelectorAll(['data-current-operand']);
